e12bfab9389a5a7c6d5023f417c30e9e3a61cb7f
Jhenifer Dias <jheniferdias1@outlook.com>
2023-03-19 21:56:26 -0300

    Adicionando transição entre telas

--- DesignCodeiOS15Study/DesignCodeiOS15StudyApp.swift
+++ DesignCodeiOS15Study/DesignCodeiOS15StudyApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct DesignCodeiOS15StudyApp: App {
     var body: some Scene {
         WindowGroup {
-            MatchedView()
+            ContentView()
         }
     }
 }
--- DesignCodeiOS15Study/Views/HomeView.swift
+++ DesignCodeiOS15Study/Views/HomeView.swift
@@ -9,35 +9,54 @@ import SwiftUI
 
 struct HomeView: View {
     @State var hasScrolled = false
+    @Namespace var namespace
+    @State var show = false
     
     var body: some View {
         ZStack {
             Color("Background").ignoresSafeArea()
-                
+            
             ScrollView {
-               scrollDetection
-               featured
-              
-                Color.clear.frame(height: 1000)
+                scrollDetection
+                featured
+                
+                Text("Courses".uppercased())
+                    .font(.footnote.weight(.semibold))
+                    .foregroundColor(.secondary)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal, 20)
+                
+                if !show {
+                    CourseItem(namespace: namespace, show: $show)
+                        .onTapGesture {
+                            withAnimation(.spring(response: 0.6,
+                            dampingFraction: 0.8 )) {
+                                show.toggle()
+                            }
+                        }
+                }
             }
             .coordinateSpace(name: "scroll")
-          
             .safeAreaInset(edge: .top, content: {
                 Color.clear.frame(height: 70)
             })
             .overlay(
                 NavigationBar(title: "Featured", hasScrolled: $hasScrolled)
-        )
+            )
+            
+            if show {
+                CourseView(namespace: namespace, show: $show)
+            }
         }
     }
     var scrollDetection: some View {
         GeometryReader { proxy in
-//                Text("\(proxy.frame(in: .named("scroll")).minY)")
+            //                Text("\(proxy.frame(in: .named("scroll")).minY)")
             Color.clear.preference(key: ScrollPreferenceKey.self,value: proxy.frame(in:.named("scroll")).minY)
         }
         .frame(height: 0)
         .onPreferenceChange(ScrollPreferenceKey.self, perform: { value in
-            withAnimation(.easeOut) {
+            withAnimation(.easeInOut) {
                 if value < 0 {
                     hasScrolled = true
                 } else {
@@ -66,19 +85,18 @@ struct HomeView: View {
                                 .offset(x: 32, y: -80)
                                 .offset(x: minX / 2)
                             
-                    )
+                        )
                     
-//                    Text("\(proxy.frame(in: .global).minX)")
+                    //                    Text("\(proxy.frame(in: .global).minX)")
                 }
             }
         }
         .tabViewStyle(.page(indexDisplayMode: .never))
         .frame(height: 430)
         .background(
-            Image("Blob 1").offset(x: 250, y: -100)
+            Image("Blob 1")
+                .offset(x: 250, y: -100)
         )
-
-        
     }
 }
 
@@ -86,5 +104,6 @@ struct HomeView: View {
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .preferredColorScheme(.dark)
     }
 }
